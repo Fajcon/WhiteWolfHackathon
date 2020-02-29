72fac39b543a1c2e2d0f52d1e248f4cd8346a464
Jakub Ficon <jakub.ficon@asseco.pl>
2020-02-29 18:04:33 +0100

    WIP chat

--- pizzaAngularowa/src/app/app.module.ts
+++ pizzaAngularowa/src/app/app.module.ts
@@ -8,9 +8,10 @@ import {MainHeaderComponent} from './main-header/main-header.component';
 import {MainFooterComponent} from './main-footer/main-footer.component';
 import {ListOfDoctorsComponent} from './list-of-doctors/list-of-doctors.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatRadioModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatFormFieldModule, MatCardModule, MatIconModule, MatToolbarModule} from '@angular/material';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {ChatComponent} from './chat/chat.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { FindDoktorModalComponent } from './home/find-doktor-modal/find-doktor-modal.component';
 import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {ReactiveFormsModule} from '@angular/forms';
@@ -48,6 +49,10 @@ const appRoutes: Routes = [
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
     FlexLayoutModule,
     MatCardModule,
     MatDialogModule,
